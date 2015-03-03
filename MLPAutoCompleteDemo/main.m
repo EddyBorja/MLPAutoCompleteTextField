8b856964804575c200dbf6415520404df701ea46
Pahnev <kirill.pahnev@gmail.com>
2015-03-03 10:39:23 -0600

    Google analytics updated to v3
    Project compiles with Xcode 6 and x64 architecture

--- MLPAutoCompleteDemo/main.m
+++ MLPAutoCompleteDemo/main.m
@@ -10,15 +10,17 @@
 
 #import "DEMOAppDelegate.h"
 #import "GAI.h"
+#import "GAIDictionaryBuilder.h"
+#import "GAIFields.h"
+
 #import <sys/utsname.h>
 
 int main(int argc, char *argv[])
 {
-    @autoreleasepool {
+    @autoreleasepool
+    {
         NSString *device =
-        [[UIDevice currentDevice] respondsToSelector:@selector(uniqueIdentifier)] ?
-        [[UIDevice currentDevice] performSelector:@selector(uniqueIdentifier)] :
-        nil;
+            [[UIDevice currentDevice] respondsToSelector:@selector(uniqueIdentifier)] ? [[UIDevice currentDevice] performSelector:@selector(uniqueIdentifier)] : nil;
         struct utsname systemInfo;
         uname(&systemInfo);
         NSString *model = [NSString stringWithCString:systemInfo.machine
@@ -26,10 +28,13 @@ int main(int argc, char *argv[])
         [GAI sharedInstance].trackUncaughtExceptions = YES;
         [GAI sharedInstance].dispatchInterval = 0;
         [[GAI sharedInstance] trackerWithTrackingId:@"UA-41220983-2"];
-        [[GAI sharedInstance].defaultTracker sendEventWithCategory:model
-                                                        withAction:@"Demo Launch"
-                                                         withLabel:device
-                                                         withValue:nil];
+
+        [[GAI sharedInstance].defaultTracker send:[[[GAIDictionaryBuilder createEventWithCategory:model
+                                                                                           action:@"Demo Launch"
+                                                                                            label:device
+                                                                                            value:nil]
+                                                         set:@"start"
+                                                      forKey:kGAISessionControl] build]];
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([DEMOAppDelegate class]));
     }
 }
